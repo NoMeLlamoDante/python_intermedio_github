7d38e2d916effe1257af57e47df3491be3314a83
dante <eo.zm24@outlook.com>
2022-05-11 19:33:23 -0500

    Ejemplo de alias

--- Clase4/modulos/main.py
+++ Clase4/modulos/main.py
@@ -5,10 +5,13 @@
 #from saludos import saludo_nombre
 #saludo_nombre("dante")
 
-from saludos import *
+#from saludos import *
+#saludo()
+#saludo_nombre("Dante")
+#saludo_nombre_completo("Edgar", "Zarate")
 
-saludo()
-
-saludo_nombre("Dante")
-
-saludo_nombre_completo("Edgar", "Zarate")
+#ejemplo de alias
+#from saludos_miguel import saludo as saludo_1
+#from saludos_sarai import saludo as saludo_2
+#
+#saludo 1
