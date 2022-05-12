6470b5028acc2c017df349287fba40af8654c45e
dante <eo.zm24@outlook.com>
2022-05-11 19:29:59 -0500

    ejemplo importación global

--- Clase4/modulos/main.py
+++ Clase4/modulos/main.py
@@ -2,6 +2,13 @@
 #saludos.saludo()
 #print(saludos.nombre)
 
-from saludos import saludo_nombre
+#from saludos import saludo_nombre
+#saludo_nombre("dante")
 
-saludo_nombre("dante")
+from saludos import *
+
+saludo()
+
+saludo_nombre("Dante")
+
+saludo_nombre_completo("Edgar", "Zarate")
