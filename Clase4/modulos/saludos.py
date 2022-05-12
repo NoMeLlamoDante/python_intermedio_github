c842f2626af61314322dfb8b0777806d2997331c
dante <eo.zm24@outlook.com>
2022-05-11 19:24:05 -0500

    se agregaron multiples opciones de saludo

--- Clase4/modulos/saludos.py
+++ Clase4/modulos/saludos.py
@@ -1,4 +1,12 @@
 def saludo():
     print("hola")
 
+def saludo_nombre(nombre):
+    print(f"hola {nombre}")
+
+def saludo_nombre_completo(nombre, apellido_paterno, apellido_materno=""):
+    print(f"hola {nombre} {apellido_paterno} {apellido_materno}")
+
+
+
 nombre = "nombre"
