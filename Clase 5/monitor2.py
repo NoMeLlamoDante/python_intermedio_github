5a2e568be33fcc1cad2ee28320098980de1269e8
dante <eo.zm24@outlook.com>
2022-05-16 19:46:07 -0500

    Creada actividad monitor 2

--- Clase 5/monitor2.py
+++ Clase 5/monitor2.py
@@ -2,3 +2,25 @@
 # crear una carpeta donde se almacenaran los logs 
 # crear un archivo con X nombre 
 # insertar la informacion recabada en el archivo
+
+
+# Imports
+import psutil
+import os
+
+#datos del CPU
+cpu_nucleos = psutil.cpu_count()
+cpu_frecuencia = psutil.cpu_freq()
+
+#Memoria
+memoria_virtual = psutil.virtual_memory()
+
+#Disco Duro
+disco_uso = psutil.disk_usage('/')
+
+
+#crear Carpeta
+os.mkdir(f"logs")
+
+with open("logs/log.txt", "w") as archivo: 
+    archivo.write(f"Datos de PC\n cantida de nucleos: {cpu_nucleos} \nfrecuencia: {cpu_frecuencia} \n memoria Virtual: {memoria_virtual}\n Disco duro: \n")
